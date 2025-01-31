c6c8d84b525ebe3763be0f452694efe1d7fa63f8
edilsonchaves <edilson.montenegro@gmail.com>
2025-01-31 00:28:05 -0300

    Create a vfx when dummy was hurt

--- Assets/_TeamAssets/Scripts/Avatar/AvatarStateManager.cs
+++ Assets/_TeamAssets/Scripts/Avatar/AvatarStateManager.cs
@@ -98,6 +98,7 @@ public class AvatarStateManager : MonoBehaviour
         if (other.gameObject.CompareTag("Enemy"))
         {
             IncreaseSpecialPoint();
+            other.GetComponent<DummyStateManager>()?.ReceiveDamage();
         }
     }
 
---
+++ Assets/_TeamAssets/Scripts/Dummy/DummyIdleState.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class DummyIdleState : DummyBaseState
+{
+    public override void EnterState(DummyStateManager dummy)
+    {
+    }
+
+    public override void ExitState(DummyStateManager dummy)
+    {
+    }
+
+    public override void UpdateState(DummyStateManager dummy)
+    {
+    }
+}
