f2706d27ef0f3e95a64c32076d03df17b7cc899c
edilsonchaves <edilson.montenegro@gmail.com>
2025-01-31 04:08:51 -0300

    Fix special bar bug

--- Assets/_TeamAssets/Scripts/UI/SpecialPointBar.cs
+++ Assets/_TeamAssets/Scripts/UI/SpecialPointBar.cs
@@ -7,18 +7,19 @@ public class SpecialPointBar : MonoBehaviour
     [SerializeField] private Image _currentSpecialPointBar;
     [SerializeField] private TextMeshProUGUI _specialPointText;
     [SerializeField] private AudioSource _audioSource;
-
+    private const int FULL_SPECIAL_BAR = 1;
     public void SetSpecialPointBar(float currentAvatarSpecialPoint, float maxAvatarSpecialPoint, bool canUseSpecialAttack)
     {
-        var relativeSpecialPoint = currentAvatarSpecialPoint / maxAvatarSpecialPoint;
-        _currentSpecialPointBar.transform.localScale = new Vector3(relativeSpecialPoint, _currentSpecialPointBar.transform.localScale.y, _currentSpecialPointBar.transform.localScale.z);
         if(currentAvatarSpecialPoint < maxAvatarSpecialPoint)
         {
+            var relativeSpecialPoint = currentAvatarSpecialPoint / maxAvatarSpecialPoint;
+            _currentSpecialPointBar.transform.localScale = new Vector3(relativeSpecialPoint, _currentSpecialPointBar.transform.localScale.y, _currentSpecialPointBar.transform.localScale.z);
             _specialPointText.text = $"{currentAvatarSpecialPoint} / {maxAvatarSpecialPoint}";
 
         }
         else
         {
+            _currentSpecialPointBar.transform.localScale = new Vector3(FULL_SPECIAL_BAR, _currentSpecialPointBar.transform.localScale.y, _currentSpecialPointBar.transform.localScale.z);
             _specialPointText.text = $"Special Attack";
             if(!canUseSpecialAttack)
                 _audioSource.Play();
